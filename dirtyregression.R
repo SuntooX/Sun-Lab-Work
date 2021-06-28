3888725c3a1a2c64aa2e91155a737a2372d342a8
Alexander <54440961+SuntooX@users.noreply.github.com>
2021-06-28 09:48:37 -0400

    Add files via upload

--- dirtyregression.R
+++ dirtyregression.R
@@ -1,67 +1,65 @@
 # Generate Starting Sequences from Ranges
-a <- append(a, runif(s, min(a), max(a)))
-b <- append(b, runif(s, min(b), max(b)))
-c <- append(c, runif(s, min(c), max(c)))
-#d <- append(d, runif(s, min(d), max(d)))
-all.runs <- transpose(as.data.frame(1:7))
-colnames(all.runs) <- c("activename", "a","b","c","d","RMSEscore","Iterations")
+all.runs <- as.data.frame(transpose(as.data.frame(1:6)))
+colnames(all.runs) <- c("activename", "a","b","c","RMSE","Iterations")
 
 # start of looping madness
-for ( u in z) {
+for (u in z) {
   u <- as.numeric(u)
   {  
-  tempvector <- a
-    source(file = "range.randomizer.R", local = TRUE)
-  #a can not equal 0 nor 1
-  a_list <- range[range > 0]
-  a_list <- a_list[a_list < 1]
-  
-  tempvector <- b
-    source(file = "range.randomizer.R", local = TRUE)
-  #b can not equal 0
-  b_list <- range[range > 0]
-  b_list <- b_list[!b_list > 1]
-  
-  tempvector <- c
-    source(file = "range.randomizer.R", local = TRUE)
-  c_list <- range
-  c_list[c_list < 0] <- c_list[c_list < 0]*-1 
-#  tempvector <- d 
-#    source(file = "range.randomizer.R", local = TRUE)
-#  d <- sample(range, 12, replace = TRUE)
+    range.input <- a
+    source(file = paste(randomizer), local = TRUE)
+    #a can not equal 0 nor 1
+    a_list <- as.numeric(range[range > 0])
+    a_list <- a_list[a_list < 1]
+    
+    range.input <- b
+    source(file = paste(randomizer), local = TRUE)
+    #b can not equal 0
+    b_list <- as.numeric(range[range > 0])
+
+    range.input <- c
+    source(file = paste(randomizer), local = TRUE)
+    c_list <- as.numeric(range[range > 0]) 
+    c_list <- as.vector(c_list)
+    
   }
-  tempvector <- c()
-  results <- transpose(as.data.frame(1:7))
-  colnames (results) <- c("activename","a","b","c","d","RMSEscore","Iterations")
-  for (a in a_list){
+  results <- as.data.frame(transpose(as.data.frame(1:6)))
+  colnames(results) <- c("activename","a","b","c","RMSE","Iterations")
+  for (a in a_list){"range.randomizer.R"
+    if (a >= max(x)) {a <- rnorm(1, mean = mean(x/10), 1)} else {}
+    if (a <= min(x)/10) {a <- rnorm(1, mean = mean(x/10), 1)} else {}
+    a <- as.numeric(prettyNum(a, digits=4))
     for (b in b_list){
+      if (b >= max(x)) {b <- rnorm(1, mean = mean(x), 1)} else {}
+      if (b <= min(x)/10) {b <- rnorm(1, mean = mean(x), 1)} else {}
+      b <- as.numeric(prettyNum(b, digits=4))
       for (c in c_list){
-        #for (d in l){
-          predict_y <- c()
-          predict_y <- activefunction(x)
-          predict_y[is.na(predict_y)] <- 0
-          tempterm <- abs(RMSE(y, predict_y))
-          RMSEscore <- abs(tempterm)
-          tempvector <- c(ActiveName, a, b, c, d, RMSEscore, u)
-          results <- rbind(results, tempvector)
-          #}
-        } 
-     }
+        if (c >= max(x)) {c <- rnorm(1, mean = mean(x), 1)} else {}
+        if (c <= min(x)/10) {c <- rnorm(1, mean = mean(x), 1)} else {}
+        c <- as.numeric(prettyNum(c, digits=4))
+        predicted <- c() 
+        predicted <- activefunction(x)
+        predicted[is.na(predicted)] <- 0
+        RMSE <- prettyNum(rmse(y, predicted), digits = 4)
+        tempvector3 <- c(ActiveName, a, b, c, RMSE, u)
+        results <- rbind(results, tempvector3)
+        results <- results[order(results$RMSE), ]
+        results <- results[!duplicated(results[,c("activename", "a")]),]
+      } 
+    }
   }
   {
-  results <- subset(results, !is.na(results$RMSEscore))
-  results <- subset(results, !is.infinite(results$RMSEscore))
-  all.runs <- rbind(all.runs, results)
-  
-  results <- results[order(results$RMSEscore), ] [1:6,]
-  a <- as.vector(results$a)
-  b <- as.vector(results$b)
-  c <- as.vector(results$c)
-  #d <- as.vector(results$d)
-  RMSE <- as.vector(results$RMSEscore)
+    results <- subset(results, !is.na(results$RMSE))
+    results <- subset(results, !is.infinite(results$RMSE))
+    results <- distinct(results, .keep_all = FALSE)
+    all.runs <- rbind(all.runs, results)
+    
+    range.best.six <- results[order(results$RMSE), ][1:6,]
+    a <- as.vector(range.best.six$a)
+    b <- as.vector(range.best.six$b)
+    c <- as.vector(range.best.six$c)
+    RMSE <- as.vector(range.best.six$RMSE)
   }
 }
 
 all.runs <- rbind(all.runs, results)
-results <- results[order(results$RMSEscore), ] [1:6,]
-
